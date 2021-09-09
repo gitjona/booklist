cb198da6fc2c18461f03b4f79424c388caa13ee1
gitjona <89576059+gitjona@users.noreply.github.com>
2021-09-09 14:34:50 +0300

    Add files via upload

---
+++ booklist/src/main/java/fi/haagahelia/course/BookListApplication.java
@@ -0,0 +1,41 @@
+package fi.haagahelia.course;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
+
+@SpringBootApplication
+public class BookListApplication 
+{
+	private static final Logger log = LoggerFactory.getLogger(BookListApplication.class);
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(BookListApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository)
+	{
+		return (args) -> 
+		{
+			log.info("save a couple of books");
+			repository.save(new Book("Ernest Hemingway", "A Farawell to Arms", "1232323-21", "1929"));
+			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", "1945"));	
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll())
+			{
+				log.info(book.toString());
+			}
+
+		};
+	}
+}
